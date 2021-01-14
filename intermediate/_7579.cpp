00375ec578c19bff866cd62a3553d2587eb5b5cb
Jasper <jasper695@icloud.com>
2021-01-14 22:15:13 +0900

    _fail

--- intermediate/_7579.cpp
+++ intermediate/_7579.cpp
@@ -1,7 +1,4 @@
-#include <algorithm>
-#include <climits>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -29,12 +26,11 @@ int main(void)
     }
     // dp[i][j] == ith 앱까지 확인시 최대 j의 cost로 비활성화 할 수 있는 확보가능한 최대 메모리
     // j를 출력해야 한다.
-    vector<vector<int> > dp(running_app_nun + 1, vector<int>(required_memory, 0));
+    int dp[MAX_APP_NUM][MAX_COST] = {0, };
     for (size_t i = 1; i <= running_app_nun; i++)
     {
-        for (int j = 0; j <= cost_sum; j++)
+        for (int j = cost[i]; j <= cost_sum; j++)
         {
-            if(j < cost[i]) continue;
             dp[i][j] = max(dp[i - 1][j - cost[i]] + memory[i], dp[i - 1][j]);
         }
     }
