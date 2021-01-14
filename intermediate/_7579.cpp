4a16b3fd7ba32a572c1dcc6151517124ca05ecc1
Jasper <jasper695@icloud.com>
2021-01-14 23:51:41 +0900

    코드 수정 및 추가

--- intermediate/_7579.cpp
+++ intermediate/_7579.cpp
@@ -18,26 +18,25 @@ int main(void)
     size_t running_app_num;
     priority_queue<int, vector<int>, greater<int> > q;
     int cost_sum = 0, required_memory;
+    int memory[MAX_APP_NUM] = {0, }, cost[MAX_APP_NUM] = { 0, };
     cin >> running_app_num >> required_memory;
-    vector<pair<int, int> > cost_memory(running_app_num + 1);
     for (size_t i = 1; i <= running_app_num; i++)
     {
-        cin >> cost_memory[i].second;
+        cin >> memory[i];
     }
     for (size_t i = 1; i <= running_app_num; i++)
     {
-        cin >> cost_memory[i].first;
-        cost_sum += cost_memory[i].first;
+        cin >> cost[i];
+        cost_sum += cost[i];
     }
-    sort(cost_memory.begin() + 1, cost_memory.end());
     // dp[i][j] == (cost기준으로 정렬된 배열에서)ith 앱까지 확인시 최대 j의 cost로 비활성화 할 수 있는 확보가능한 최대 메모리
     // j를 출력해야 한다.
     int dp[MAX_APP_NUM][MAX_COST] = { 0 };
     for (size_t i = 1; i <= running_app_num; i++)
     {
-        for (int j = cost_memory[i].first; j <= cost_sum; j++)
+        for (int j = cost[i]; j <= cost_sum; j++)
         {
-            dp[i][j] = max(dp[i - 1][j - cost_memory[i].first] + cost_memory[i].second, dp[i - 1][j]);
+            dp[i][j] = max(dp[i - 1][j - cost[i]] + memory[i], dp[i - 1][j]);
         }
     }
     for (size_t i = 1; i <= running_app_num; i++)
