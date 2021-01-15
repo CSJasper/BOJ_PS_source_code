97ecd10e0b24b90a06510016cc7075dec00ce23d
Jasper <jasper695@icloud.com>
2021-01-15 19:58:47 +0900

    코드 수정

--- intermediate/_7579.cpp
+++ intermediate/_7579.cpp
@@ -18,7 +18,7 @@ int main(void)
     size_t running_app_num;
     priority_queue<int, vector<int>, greater<int> > q;
     int cost_sum = 0, required_memory;
-    int memory[MAX_APP_NUM] = {0, }, cost[MAX_APP_NUM] = { 0, };
+    int memory[MAX_APP_NUM] = { 0, }, cost[MAX_APP_NUM] = { 0, };
     cin >> running_app_num >> required_memory;
     for (size_t i = 1; i <= running_app_num; i++)
     {
@@ -29,14 +29,32 @@ int main(void)
         cin >> cost[i];
         cost_sum += cost[i];
     }
+    // 
     // dp[i][j] == (cost기준으로 정렬된 배열에서)ith 앱까지 확인시 최대 j의 cost로 비활성화 할 수 있는 확보가능한 최대 메모리
     // j를 출력해야 한다.
-    int dp[MAX_APP_NUM][MAX_COST] = { 0 };
+    int dp[MAX_APP_NUM][MAX_COST] = { 0, };
     for (size_t i = 1; i <= running_app_num; i++)
     {
-        for (int j = cost[i]; j <= cost_sum; j++)
-        {
-            dp[i][j] = max(dp[i - 1][j - cost[i]] + memory[i], dp[i - 1][j]);
+        for (int j = 0; j <= cost_sum; j++)
+        {   
+            // 만약 1번째 앱까지 본다면 cost[1]이상만 되도 걍 memory[1]이 되겠다.
+            if(i == 0)
+            {
+                if(j >= cost[i]) dp[i][j] = memory[i];
+            }
+            else
+            {
+                if(j >= cost[i])
+                {
+                    dp[i][j] = max(dp[i - 1][j - cost[i]] + memory[i], dp[i - 1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+            
         }
     }
     for (size_t i = 1; i <= running_app_num; i++)
