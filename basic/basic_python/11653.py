53477dcb5f87b9859dfa04f01eec87926c37e3de
Jasper <jasper695@icloud.com>
2021-03-14 12:57:27 +0900

    src code

---
+++ basic/basic_python/11653.py
@@ -0,0 +1,6 @@
+import sys
+
+MAX_NUM = int(10e7)
+
+num = int(sys.stdin.readline())
+#
--- basic/basic_python/2581.py
+++ basic/basic_python/2581.py
@@ -1,2 +1,35 @@
 import sys
 
+
+def is_prime(number):
+    if number == 1:
+        return False
+    i = 2
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        else:
+            pass
+        i += 1
+    return True
+
+
+lBound = int(sys.stdin.readline())
+uBound = int(sys.stdin.readline())
+primeCount = 0
+primeSum = 0
+prime = []
+
+for num in range(lBound, uBound + 1):
+    if is_prime(num) is True:
+        primeCount += 1
+        primeSum += num
+        prime.append(num)
+    else:
+        continue
+
+if primeCount == 0:
+    print(-1)
+else:
+    print(primeSum)
+    print(min(prime))
