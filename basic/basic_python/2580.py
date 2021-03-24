50cb8c8d650d66fb793e4297f1695ae57556b077
Jasper <jasper695@icloud.com>
2021-03-24 11:16:43 +0900

    sudoku

--- basic/basic_python/2580.py
+++ basic/basic_python/2580.py
@@ -1,2 +1,17 @@
 import sys
 
+SUDOKU_SIZE = 9
+
+sudoku = [[] for i in range(SUDOKU_SIZE)]
+
+for i in range(SUDOKU_SIZE):
+    sudoku[i] = list(map(int, sys.stdin.readline().split()))
+
+for i in range(SUDOKU_SIZE):
+    for j in range(SUDOKU_SIZE):
+        if sudoku[i][j] != 0:
+            continue
+        # 만약 0이라면 3가지 조건을 만족하는지 확인한다.
+        for candidate in range(1,10):
+            if candidate not in sudoku[i]:  # 이러면 가로줄에 candidate 는 위배되지 않게 올 수 있는것!
+
